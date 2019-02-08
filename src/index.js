7618d36e263721467ca1db8e3612209976625382
dhershman1 <dustinh17@gmail.com>
2019-02-08 12:49:27 -0500

    Added listenTo option so you can set a custom event if needed

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import debounce from './debounce'
 
 export default {
   name: 'debounce',
-  install (Vue, { lock } = {}) {
+  install (Vue, { lock, listenTo = 'onkeyup' } = {}) {
     Vue.directive('debounce', {
       bind (el, { value, arg, modifiers }) {
+        if (typeof el[listenTo] === 'undefined') {
+          throw new Error(`Event Listener ${listenTo} does not exist`)
+        }
         const fn = debounce(target => {
           value(target.value)
         }, arg)
         const isUnlocked = (!modifiers.lock && !lock) || modifiers.unlock
 
-        el.onkeyup = ({ keyCode, target }) => {
+        el[listenTo] = ({ keyCode, target }) => {
           if (keyCode === 13 && isUnlocked) {
             fn.cancel()
             value(target.value)
