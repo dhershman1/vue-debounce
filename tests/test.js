7b37321ebc7f2058af6785444b70268cb7680551
dhershman1 <dhershman@budgetdumpster.com>
2019-09-23 09:41:20 -0400

    added number type test

--- tests/test.js
+++ tests/test.js
@@ -11,6 +11,16 @@ test('Handles defaults', t => {
   runner('testing')
 })
 
+test('Handles number type', t => {
+  const runner = debounce(val => {
+    t.ok(val)
+    t.same(val, 'testing')
+    t.end()
+  }, 400)
+
+  runner('testing')
+})
+
 test('Handles no format set', t => {
   const runner = debounce(val => {
     t.ok(val)
