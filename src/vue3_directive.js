85e993ae4ecd8b2239f0d184fc020ad4c41759c7
dhershman1 <dustinh17@gmail.com>
2022-05-17 14:51:29 -0400

    Added: Vue 3 dedicated directive

---
+++ src/vue3_directive.js
@@ -0,0 +1,47 @@
+import debounce from './debounce'
+import { mapOutListeningEvents, isEmpty, isLocked, shouldFireOnEmpty } from './_internals'
+
+export default function ({
+  lock = false,
+  listenTo = 'keyup',
+  defaultTime = '300ms',
+  fireOnEmpty = false,
+  cancelOnEmpty = false,
+  trim = false
+} = {}) {
+  return {
+    created (el, {
+      value: debouncedFn,
+      arg: timer = defaultTime,
+      modifiers
+    }, vnode) {
+      const combinedRules = Object.assign({
+        lock,
+        trim,
+        fireonempty: fireOnEmpty,
+        cancelonempty: cancelOnEmpty
+      }, modifiers)
+      const events = mapOutListeningEvents(vnode.props, listenTo)
+      const fn = debounce(e => {
+        debouncedFn(e.target.value, e)
+      }, timer)
+
+      function handler (event) {
+        const value = combinedRules.trim ? event.target.value.trim() : event.target.value
+
+        if (isEmpty(value) && combinedRules.cancelonempty) {
+          fn.cancel()
+        } else if (isLocked(event.key, combinedRules) || shouldFireOnEmpty(value, event.key, combinedRules)) {
+          fn.cancel()
+          debouncedFn(event.target.value, event)
+        } else {
+          fn(event)
+        }
+      }
+
+      events.forEach(e => {
+        el.addEventListener(e, handler)
+      })
+    }
+  }
+}
