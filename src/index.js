d459ad00c92be2e5bb0fb93aed1a75004e994f4c
dhershman1 <dustinh17@gmail.com>
2023-12-03 17:02:17 -0500

    Updated: export default only

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import debounce from './debounce'
 import vueDebounce from './directive'
-import vue3Debounce from './vue3_directive'
-
-export {
-  debounce,
-  vue3Debounce
-}
 
 export default {
-  install (Vue, opts = {}) {
-    Vue.directive('debounce', vueDebounce(opts))
-  }
+  debounce,
+  vueDebounce
 }
