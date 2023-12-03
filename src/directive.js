ed3b8c3a8a85271ea90efa58062a23fdad25859d
dhershman1 <dustinh17@gmail.com>
2023-12-03 17:01:37 -0500

    Replaced: vue3_directive with directive js

--- src/directive.js
+++ src/directive.js
@@ -10,7 +10,7 @@ export default function ({
   trim = false
 } = {}) {
   return {
-    bind (el, {
+    created (el, {
       value: debouncedFn,
       arg: timer = defaultTime,
       modifiers
@@ -21,7 +21,7 @@ export default function ({
         fireonempty: fireOnEmpty,
         cancelonempty: cancelOnEmpty
       }, modifiers)
-      const events = mapOutListeningEvents(vnode.data.attrs, listenTo)
+      const events = mapOutListeningEvents(vnode.props, listenTo)
       const fn = debounce(e => {
         debouncedFn(e.target.value, e)
       }, timer)
